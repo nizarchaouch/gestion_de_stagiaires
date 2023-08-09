ea2d4827fced692bd2c8e9fe71a8586337d1c25d
Chaouch Nizar <chaouchnizar1@gmail.com>
2023-08-09 13:51:28 +0100

    SCROLMODAL

--- src/components/ModalAjoutStag.vue
+++ src/components/ModalAjoutStag.vue
@@ -1,10 +1,10 @@
 <template>
-  <!-- Button trigger modal -->
   <button
     type="button"
     class="btn btn-primary"
     data-bs-toggle="modal"
     data-bs-target="#exampleModal"
+    data-bs-whatever="@getbootstrap"
   >
     Ajouter
     <svg
@@ -23,7 +23,6 @@
     </svg>
   </button>
 
-  <!-- Modal -->
   <div
     class="modal fade"
     id="exampleModal"
@@ -31,10 +30,10 @@
     aria-labelledby="exampleModalLabel"
     aria-hidden="true"
   >
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-scrollable">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Ajouté Stagiaire</h5>
           <button
             type="button"
             class="btn-close"
@@ -42,23 +41,68 @@
             aria-label="Close"
           ></button>
         </div>
-        <div class="modal-body">...</div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="nom&prenom-name" class="col-form-label"
+                >Nom Prenom :</label
+              >
+              <input type="text" class="form-control" id="nom&prenom-name" />
+            </div>
+            <div class="mb-3">
+              <label for="gmail-name" class="col-form-label">Gmail :</label>
+              <input type="email" class="form-control" id="gmail-name" />
+            </div>
+            <div class="mb-3">
+              <label for="tel-name" class="col-form-label">Telephone :</label>
+              <input type="number" class="form-control" id="tel-name" />
+            </div>
+            <div class="mb-3">
+              <label for="dated-name" class="col-form-label"
+                >Date de debuit :</label
+              >
+              <input type="date" class="form-control" id="dated-name" />
+            </div>
+            <div class="mb-3">
+              <label for="datef-name" class="col-form-label"
+                >Date de fin :</label
+              >
+              <input type="date" class="form-control" id="datef-name" />
+            </div>
+            <div class="mb-3">
+              <label for="encadrant-name" class="col-form-label"
+                >Encadrant :</label
+              >
+              <input type="text" class="form-control" id="encadrant-name" />
+            </div>
+            <div class="mb-3">
+              <label for="typestage-name" class="col-form-label"
+                >Type de stage :</label
+              >
+              <input type="text" class="form-control" id="typestage-name" />
+            </div>
+            <div class="mb-3">
+              <label for="message-text" class="col-form-label">Message:</label>
+              <textarea class="form-control" id="message-text"></textarea>
+            </div>
+          </form>
+        </div>
         <div class="modal-footer">
           <button
             type="button"
             class="btn btn-secondary"
             data-bs-dismiss="modal"
           >
-            Close
+            Ferme
           </button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-primary">Ajouter</button>
         </div>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
-button {
+.btn {
   color: aliceblue;
   display: inline-flex;
   padding: 7px 15px;
