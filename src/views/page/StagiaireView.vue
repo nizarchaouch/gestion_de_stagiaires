78238daf400d3a0824a0059195e01b058155b5c3
Chaouch Nizar <chaouchnizar1@gmail.com>
2023-08-10 10:13:58 +0100

    modifier pagination

--- src/views/page/StagiaireView.vue
+++ src/views/page/StagiaireView.vue
@@ -1,13 +1,15 @@
 <template>
   <modalA />
   <tableS></tableS>
+  <pagination />
 </template>
 
 <script>
 import modalA from "@/components/ModalAjoutStag.vue";
 import tableS from "@/components/tablestag/TableStag.vue";
+import pagination from "@/components/pagination/PaginationC.vue";
 export default {
-  components: { modalA, tableS },
+  components: { modalA, tableS, pagination },
   setup() {},
 };
 </script>
