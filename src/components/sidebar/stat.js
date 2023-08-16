cee9b216aed24b22c62b0a48f897c8545d86c39d
Chaouch Nizar <chaouchnizar1@gmail.com>
2023-08-16 14:26:18 +0100

    modifier width side bar

--- src/components/sidebar/stat.js
+++ src/components/sidebar/stat.js
@@ -4,7 +4,7 @@ export const collapsed = ref(true);
 export const toggleSidebar = () => (collapsed.value = !collapsed.value);
 
 export const SIDEBAR_WIDTH = 230;
-export const SIDEBAR_WIDTH_COLLAPSED = 60;
+export const SIDEBAR_WIDTH_COLLAPSED = 70;
 export const sidebarWidth = computed(
   () => `${collapsed.value ? SIDEBAR_WIDTH_COLLAPSED : SIDEBAR_WIDTH}px`
 );
