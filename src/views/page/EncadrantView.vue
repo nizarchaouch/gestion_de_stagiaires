bb8d04fa30a94b2cff9d74fee3dfa03f0df6d0fd
Chaouch Nizar <chaouchnizar1@gmail.com>
2023-09-06 18:53:04 +0100

    modifier encadrant view

--- src/views/page/EncadrantView.vue
+++ src/views/page/EncadrantView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="float-start ms-5"><h4>Liste Encadrant</h4></div>
+  <div class="float-start ms-5"><h4>Liste Encadreur</h4></div>
   <div class="float-end me-5"><ModalA /></div>
   <TableE />
 </template>
